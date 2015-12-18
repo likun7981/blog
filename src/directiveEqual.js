940e11906fb0477713bdf5b4f31123c930b02b7c
likun1117 <likun7981@gmail.com>
2015-12-18 10:57:36 +0800

    上传源代码

--- src/directiveEqual.js
+++ src/directiveEqual.js
@@ -1,12 +1,12 @@
 angular.module('MyApp',[])
 .controller('MyController',['$scope',function(scope){
-	scope.ctrlVar = {name:'hello world'};
+	scope.ctrlVar = {name:'hello'};
 }])
 .directive('hello',function(){
 	return{
 		restrict:'AE',
 		scope:{
-			abc:'@myvar'
+			abc:'=myvar'
 		},
 		repalce:true,
 		template:'<input class="form-control" type="text" ng-model="abc" />'
